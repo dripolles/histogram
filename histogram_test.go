eebe49746d5b27e5047479a3b996be5bbdde6407
Daniel Ripolles <dripolles@gmail.com>
2015-05-11 12:56:40 +0200

    Better tests, including error conditions

--- histogram_test.go
+++ histogram_test.go
@@ -24,32 +24,33 @@ func (s *HistogramSuite) TestHistogram_Len(c *C) {
 }
 
 func (s *HistogramSuite) TestHistogram_Get(c *C) {
-	values := []int{100, 100, 500, 500, 500, 500, 900, 900, 900, 1000}
-	h := NewHistogram()
-	for _, v := range values {
-		h.Add(v)
-	}
+	h := createSampleHistogram()
 
-    assertHistogramGet(c, h, 100, 2.0)
-    assertHistogramGet(c, h, 300, 3.0)
-    assertHistogramGet(c, h, 500, 4.0)
-    assertHistogramGet(c, h, 700, 3.5)
-    assertHistogramGet(c, h, 900, 3.0)
-    assertHistogramGet(c, h, 1000, 1.0)
+	assertHistogramGet(c, h, 100, 2.0)
+	assertHistogramGet(c, h, 300, 3.0)
+	assertHistogramGet(c, h, 500, 4.0)
+	assertHistogramGet(c, h, 700, 3.5)
+	assertHistogramGet(c, h, 900, 3.0)
+	assertHistogramGet(c, h, 1000, 1.0)
 }
 
-func assertHistogramGet(c*C, h*Histogram, v int, expected float64) {
-    res, err := h.Get(v)
-    c.Assert(err, IsNil)
-    c.Assert(res, Equals, expected)
+func (s *HistogramSuite) TestHistogram_GetOutOfBounds(c *C) {
+	h := createSampleHistogram()
+
+	_, err := h.Get(99)
+	c.Assert(err, DeepEquals, ExtrapolationError)
+	_, err = h.Get(1001)
+	c.Assert(err, DeepEquals, ExtrapolationError)
+}
+
+func assertHistogramGet(c *C, h *Histogram, v int, expected float64) {
+	res, err := h.Get(v)
+	c.Assert(err, IsNil)
+	c.Assert(res, Equals, expected)
 }
 
 func (s *HistogramSuite) TestHistogram_Percentile(c *C) {
-	values := []int{100, 100, 500, 500, 500, 500, 900, 900, 900, 1000}
-	h := NewHistogram()
-	for _, v := range values {
-		h.Add(v)
-	}
+	h := createSampleHistogram()
 
 	c.Assert(h.GetPercentile(0), Equals, 0.0)
 	c.Assert(h.GetPercentile(100), Equals, 0.2)
@@ -59,6 +60,16 @@ func (s *HistogramSuite) TestHistogram_Percentile(c *C) {
 	c.Assert(h.GetPercentile(900), Equals, 0.9)
 	c.Assert(h.GetPercentile(1000), Equals, 1.0)
 	c.Assert(h.GetPercentile(1001), Equals, 1.0)
+}
+
+func createSampleHistogram() *Histogram {
+	values := []int{100, 100, 500, 500, 500, 500, 900, 900, 900, 1000}
+	h := NewHistogram()
+	for _, v := range values {
+		h.Add(v)
+	}
+
+	return h
 }
 
 func (s *HistogramSuite) TestHistogram_PercentileRandom(c *C) {

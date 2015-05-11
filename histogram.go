13e85e805e7b03cec8426ee2e4d64e40decb76fc
Daniel Ripolles <dripolles@gmail.com>
2015-05-11 13:42:09 +0200

    Typo in doc

--- histogram.go
+++ histogram.go
@@ -1,4 +1,4 @@
-// Simple Histogram lib, with support for interpolation and percentile calculation.
+// Simple Histogram library, with support for interpolation and percentile calculation.
 //
 // The Histogram stores a count of the number of occurrences for each value. This count
 // can later be used to interpolate expected counts for unknown values, and to
